c5d9770d9d9534765b6c2ddb6365938920427e88
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-04-27 12:33:14 +0530

    Added routes file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,22 +1,24 @@
 import { GiCompass, GiDiamondHard, GiStabbedNote } from "react-icons/gi";
-export const services=[
-    {
-        id:1,
-        icon:<GiCompass/>,
+export const services = [{
+        id: 1,
+        icon: < GiCompass / > ,
         title: "Mission",
-        text:"Make the best in class furniture. That makes the life of your customers easy.",
+        text: "Make the best in class furniture. That makes the life of your customers easy.",
 
     },
     {
-        id:2,
-        icon:<GiDiamondHard/>,
+        id: 2,
+        icon: < GiDiamondHard / > ,
         title: "Vision",
-        text:'Bring elegance back to functional products. Why compromise? Bring harmony back to elegance and functionality.',
+        text: 'Bring elegance back to functional products. Why compromise? Bring harmony back to elegance and functionality.',
     },
     {
-        id:3,
-        icon:<GiStabbedNote/>,
+        id: 3,
+        icon: < GiStabbedNote / > ,
         title: "History",
-        text:'Expertise of tradition combined with modern technology. Respecting the past, acknowledging the present.'
+        text: 'Expertise of tradition combined with modern technology. Respecting the past, acknowledging the present.'
     }
 ]
+
+export const products_url = "https://course-api.com/react-store-products";
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
