564db4425a28c619192c260f4acf5d2ed83a080e
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-04-28 15:41:57 +0530

    Added routing for products page

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../Pages/Home";
+import Products from "../Pages/Products";
 
 
 
 const Routes = () => {
   return (
     <>
-    <Router>
+   <Router>
         <Switch>
-            <Route exact path="/">
-                <Home/>
-            </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
         </Switch>
-    </Router>
+
+      </Router>
 
     </>
   )
