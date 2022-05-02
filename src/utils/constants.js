9a3b5693e3f7d963df1f9204ee33ecfefe8d70c8
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-02 14:45:00 +0530

    Added links for routing

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,41 +1,42 @@
+import React from "react";
 import { GiCompass, GiDiamondHard, GiStabbedNote } from "react-icons/gi";
-
 export const links = [{
         id: 1,
         text: "home",
-        link: "/"
+        url: "/",
     },
     {
         id: 2,
         text: "about",
-        link: "/about"
+        url: "/about",
     },
     {
         id: 3,
         text: "products",
-        link: "/products"
-    }
-]
+        url: "/products",
+    },
+];
+
 export const services = [{
         id: 1,
         icon: < GiCompass / > ,
-        title: "Mission",
+        title: "mission",
         text: "Make the best in class furniture. That makes the life of your customers easy.",
-
     },
     {
         id: 2,
         icon: < GiDiamondHard / > ,
-        title: "Vision",
-        text: 'Bring elegance back to functional products. Why compromise? Bring harmony back to elegance and functionality.',
+        title: "vision",
+        text: "Bring elegance back to functional products. Why compromise?  Bring harmony back to elegance and functionality.",
     },
     {
         id: 3,
         icon: < GiStabbedNote / > ,
-        title: "History",
-        text: 'Expertise of tradition combined with modern technology. Respecting the past, acknowledging the present.'
-    }
-]
+        title: "history",
+        text: "Expertise of tradition combined with modern technology. Respecting the past, acknowledging the present.",
+    },
+];
 
 export const products_url = "https://course-api.com/react-store-products";
+
 export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
