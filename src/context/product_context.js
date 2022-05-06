9a1ede40a9f3fa376a46ba73f278a4ca59fd3edf
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-06 11:06:10 +0530

    Made few fixed in product context

--- src/context/product_context.js
+++ src/context/product_context.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React,{useEffect,createContext,useContext,useReducer} from 'react';
-import {products_url as url} from '../utils/constants';
+import React, { useEffect, createContext, useContext, useReducer } from 'react';
+import { products_url as url } from '../utils/constants';
 
 import {
     SIDEBAR_OPEN,
@@ -15,71 +15,70 @@ import {
 } from '../actions';
 import ProductsReducer from '../reducers/products_reducer';
 
-const initialState ={
+const initialState = {
     isSideBarOpen: false,
-    productsLoading:true,
-    productsError:true,
+    productsLoading: true,
+    productsError: true,
     products: [],
     featuredProducts: [],
-    singleProductLoading:true,
-    singleProductError:false,
-    singleProduct:[]
+    singleProductLoading: true,
+    singleProductError: false,
+    singleProduct: []
 
 }
 
-const ProductsContext=createContext();
+const ProductsContext = createContext();
 
-export const ProductsProvider=(props)=>{
-   const [state,dispatch]=useReducer(ProductsReducer,initialState);
-   const openSideBar=()=>{
-       dispatch({type:SIDEBAR_OPEN});
-   }
-   const closeSideBar=()=>{
-       dispatch({type:SIDEBAR_CLOSE});
-   }
-   const fetchProducts=async ()=>{
-       dispatch({type:GET_PRODUCTS_BEGIN});
-       try{
-       const response=await axios.get(url);
-       const products=response.data;
-       dispatch({type:GET_PRODUCTS_SUCCESS,payload:products});
-       }
-       catch{
-           dispatch({type:GET_PRODUCTS_ERROR})
-       }
-
-   }
-   const fetchSingleProduct=async (url)=>{
-       dispatch({type:GET_PRODUCTS_BEGIN});
-       try{
-           const response=await axios.get(url);
-           const singleProduct = response.data;
-           dispatch({type:GET_SINGLE_PRODUCT_SUCCESS,payload:singleProduct});
-       }
-       catch{
-           dispatch({type:GET_SINGLE_PRODUCT_ERROR});
-       }
-   }
-
-   useEffect(()=>{
-     fetchProducts(url);
-   },[]);
-
-   return(
-       <ProductsContext.Provider value={{...state,openSideBar,closeSideBar,fetchSingleProduct}}>
-       {props.children}
-
-       </ProductsContext.Provider>
-   )
-
-
-}
-
-export const useProductsContext=()=>{
-    const context=useContext(ProductsContext);
-    if(context===undefined){
-        throw new Error("useUserContext was used outside of its Provider");
+export const ProductsProvider = (props) => {
+    const [state, dispatch] = useReducer(ProductsReducer, initialState);
+    const openSideBar = () => {
+        dispatch({ type: SIDEBAR_OPEN });
     }
-    return(context);
+    const closeSideBar = () => {
+        dispatch({ type: SIDEBAR_CLOSE });
+    }
+    const fetchProducts = async() => {
+        dispatch({ type: GET_PRODUCTS_BEGIN });
+        try {
+            const response = await axios.get(url);
+            const products = response.data;
+            dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+        } catch {
+            dispatch({ type: GET_PRODUCTS_ERROR })
+        }
+
+    }
+    const fetchSingleProduct = async(url) => {
+        dispatch({ type: GET_PRODUCTS_BEGIN });
+        try {
+            const response = await axios.get(url);
+            const singleProduct = response.data;
+            dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+        } catch {
+            dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts(url);
+    }, []);
+
+    return ( <
+        ProductsContext.Provider value = {
+            {...state, openSideBar, closeSideBar, fetchSingleProduct } } > { props.children }
+
+        <
+        /ProductsContext.Provider>
+    )
+
+
+}
+
+export const useProductsContext = () => {
+    const context = useContext(ProductsContext);
+    if (context === undefined) {
+        throw new Error("useProductsContext was used outside of its Provider");
+    }
+    return (context);
 
 }
