a4ab8cf38bf20cd794a9aae51a45615131d0a525
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-07 23:12:30 +0530

    created List view component

--- src/context/product_context.js
+++ src/context/product_context.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React, { useEffect, createContext, useContext, useReducer } from 'react';
-import { products_url as url } from '../utils/constants';
+import React,{useEffect,createContext,useContext,useReducer} from 'react';
+import {products_url as url} from '../utils/constants';
 
 import {
     SIDEBAR_OPEN,
@@ -15,70 +15,71 @@ import {
 } from '../actions';
 import ProductsReducer from '../reducers/products_reducer';
 
-const initialState = {
+const initialState ={
     isSideBarOpen: false,
-    productsLoading: true,
-    productsError: true,
+    productsLoading:true,
+    productsError:true,
     products: [],
     featuredProducts: [],
-    singleProductLoading: true,
-    singleProductError: false,
-    singleProduct: []
+    singleProductLoading:true,
+    singleProductError:false,
+    singleProduct:[]
 
 }
 
-const ProductsContext = createContext();
+const ProductsContext=createContext();
 
-export const ProductsProvider = (props) => {
-    const [state, dispatch] = useReducer(ProductsReducer, initialState);
-    const openSideBar = () => {
-        dispatch({ type: SIDEBAR_OPEN });
-    }
-    const closeSideBar = () => {
-        dispatch({ type: SIDEBAR_CLOSE });
-    }
-    const fetchProducts = async() => {
-        dispatch({ type: GET_PRODUCTS_BEGIN });
-        try {
-            const response = await axios.get(url);
-            const products = response.data;
-            dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
-        } catch {
-            dispatch({ type: GET_PRODUCTS_ERROR })
-        }
+export const ProductsProvider=(props)=>{
+   const [state,dispatch]=useReducer(ProductsReducer,initialState);
+   const openSideBar=()=>{
+       dispatch({type:SIDEBAR_OPEN});
+   }
+   const closeSideBar=()=>{
+       dispatch({type:SIDEBAR_CLOSE});
+   }
+   const fetchProducts=async ()=>{
+       dispatch({type:GET_PRODUCTS_BEGIN});
+       try{
+       const response=await axios.get(url);
+       const products=response.data;
+       dispatch({type:GET_PRODUCTS_SUCCESS,payload:products});
+       }
+       catch{
+           dispatch({type:GET_PRODUCTS_ERROR})
+       }
 
-    }
-    const fetchSingleProduct = async(url) => {
-        dispatch({ type: GET_PRODUCTS_BEGIN });
-        try {
-            const response = await axios.get(url);
-            const singleProduct = response.data;
-            dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
-        } catch {
-            dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
-        }
-    }
+   }
+   const fetchSingleProduct=async (url)=>{
+       dispatch({type:GET_PRODUCTS_BEGIN});
+       try{
+           const response=await axios.get(url);
+           const singleProduct = response.data;
+           dispatch({type:GET_SINGLE_PRODUCT_SUCCESS,payload:singleProduct});
+       }
+       catch{
+           dispatch({type:GET_SINGLE_PRODUCT_ERROR});
+       }
+   }
 
-    useEffect(() => {
-        fetchProducts(url);
-    }, []);
+   useEffect(()=>{
+     fetchProducts(url);
+   },[]);
 
-    return ( <
-        ProductsContext.Provider value = {
-            {...state, openSideBar, closeSideBar, fetchSingleProduct } } > { props.children }
+   return(
+       <ProductsContext.Provider value={{...state,openSideBar,closeSideBar,fetchSingleProduct}}>
+       {props.children}
 
-        <
-        /ProductsContext.Provider>
-    )
+       </ProductsContext.Provider>
+   )
 
 
 }
 
-export const useProductsContext = () => {
-    const context = useContext(ProductsContext);
-    if (context === undefined) {
-        throw new Error("useProductsContext was used outside of its Provider");
+export const useProductsContext=()=>{
+    const context=useContext(ProductsContext);
+    if(context===undefined){
+        throw new Error("useContext was used outside of its Provider");
     }
-    return (context);
+    return(context);
 
 }
