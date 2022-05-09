8df14f58f8e5119bab7fb6874a4ad3918bf26bdf
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-09 23:28:17 +0530

    Added a case to fix the issue when there are no products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,6 +5,13 @@ import GridView from './GridView';
 
 const ProductList = () => {
     const {filteredProducts:products,gridView} = useFilters();
+    if(products.length<1){
+      return(
+        <h2 style={{textTransform:'none'}}>
+          Sorry, no products matched your search :/
+        </h2>
+      )
+    }
     if(gridView===false){
      return( <ListView products={products}/>);
     }
