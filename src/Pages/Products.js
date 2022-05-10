49c4db05905cdb444c2d0a7b2d4bb6811f42c4d2
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-10 22:51:31 +0530

    Added filters feature

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 import PageHero from '../components/PageHero';
 import Product from '../components/Product';
 import ProductList from '../components/ProductList';
+import Filters from '../components/Filters';
 import Sort from '../components/Sort';
 const Products = () => {
   return (
     <main>
 
       <PageHero title="products"/>
-      <Sort/>
-      <ProductList/>
+       <Wrapper className="page">
+       <div className="section-center products">
+         <Filters/>
+         <div>
+         <Sort/>
+         <ProductList/>
+         </div>
+      </div>
+      </Wrapper>
     </main>
   )
 }
 
+const Wrapper=styled.div`
+.products{
+  display:grid;
+  gap:3rem 1.5rem;
+  margin:4rem auto;
+}
+@media (min-width: 768px){
+  .products{
+    grid-template-columns:200px 1fr;
+  }
+
+}
+
+`;
 
 
 export default Products;
