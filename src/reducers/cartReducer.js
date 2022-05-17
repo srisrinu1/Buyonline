947d81b7ddbaae623a10f4386ea183c315bd4e2d
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-17 12:00:39 +0530

    fixed a bug in cart reducer

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -46,7 +46,7 @@ export const cartReducer = (state, action) => {
             }
         case REMOVE_CART_ITEM:
 
-            const tempCart2 = state.cart.filter((item) => item.id !== pay);
+            const tempCart2 = state.cart.filter((item) => item.id !== payload);
             return {...state, cart: tempCart2 };
         case CLEAR_CART:
             return {...state, cart: [] };
