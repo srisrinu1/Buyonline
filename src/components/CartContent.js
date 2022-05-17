4ee0bc94861903cf25eaf1e67b30ea86af475c9a
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-17 21:16:21 +0530

    Added total Amount component

--- src/components/CartContent.js
+++ src/components/CartContent.js
@@ -4,6 +4,7 @@ import {useCart} from '../context/cart_context';
 import {Link} from 'react-router-dom';
 import CartColumns from '../components/CartColumns';
 import CartItem from '../components/CartItem';
+import CartTotals from './CartTotals';
 
 const CartContent = () => {
 
@@ -20,6 +21,20 @@ const CartContent = () => {
       })}
 
       <hr />
+       <div className="link-container">
+       <Link to="/products" className="link-btn">
+          continue shopping
+        </Link>
+         <button
+         type="button"
+         className="link-btn clear-btn"
+         onClick={clearCart}>
+         clear shopping cart
+
+         </button>
+       </div>
+       <CartTotals/>
+
 
 
     </Wrapper>
@@ -27,6 +42,31 @@ const CartContent = () => {
 }
 
 const Wrapper=styled.section `
+   .link-container{
+     display:flex;
+     justify-content:space-between;
+     margin-top: 2rem;
+   }
+
+   .link-btn{
+    background: transparent;
+    border-color: transparent;
+    text-transform:capitalize;
+    padding:0.25rem 0.5rem;
+    background: var(--clr-primary-5);
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--clr-white);
+    border-radius: var(--radius);
+    letter-spacing: var(--spacing);
+    font-weight:400;
+    cursor: pointer;
+   }
+
+   .clear-btn{
+     background-color:var(--clr-black);
+   }
 `;
 
 
