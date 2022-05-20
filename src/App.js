bd3733275b9ca1c207716d170cd8e7ba2cf94bd0
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-20 23:02:38 +0530

    Tested lambda function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import Home from './Pages/Home'
 import './App.css';
 import './index.css';
 import Routes from './Routes/Routes';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [text,setText] =useState('');
+
+  const fetchData=async()=>{
+    const response =await axios.get('/.netlify/functions/hello');
+    const textData=response.data;
+    setText(textData);
+
+  }
+
+  useEffect(()=>{
+     fetchData();
+  },[]);
+
+
   return (
     <div className="App">
+
      <Routes/>
+     <p>{text}</p>
     </div>
   );
 }
