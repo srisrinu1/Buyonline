4a2647e3578d596ba35cae3fd6f0ed75621a2f95
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-23 16:25:10 +0530

    Added checkout page

---
+++ src/Pages/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Checkout = () => {
+  return (
+    <Wrapper>
+       <p>Hi</p>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section ``;
+
+export default Checkout
--- src/Pages/PrivateRoute.js
+++ src/Pages/PrivateRoute.js
@@ -1,17 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import {useUserContext} from '../context/user_context';
 
-const PrivateRoute = ({children,...routeProps}) => {
-    const {user}=useAuth0();
-  return (
-   <Route
-   render={()=>{
-       return user?children: <Redirect to="/"/>
-   }}>
+const PrivateRoute = ({ children, ...routeProps }) => {
+    const {user } = useAuth0();
 
-   </Route>
-  )
-}
 
-export default PrivateRoute
+
+    return (
+      <Route
+        {...routeProps}
+        render={() => {
+          return user ? children : <Redirect to="/" />;
+        }}
+      />
+    );
+  };
+  export default PrivateRoute;
