c865952233e144f86cfdaa8a9f069e4489d5eef6
srisrinu1 <srinivasreddychalla133@gmail.com>
2022-05-28 13:56:26 +0530

    Added orders Page for displaying orders page

--- src/Pages/Orders.js
+++ src/Pages/Orders.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from 'react';
+import PageHero from '../components/PageHero';
+import OrderItem from '../components/OrderItem';
+import {useCart} from '../context/cart_context';
+import {formatPrice} from '../utils/helpers';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
 
 const Orders = () => {
+  const {orders,perOrderTotal}=useCart();
+  if(orders?.length > 0) {
   return (
-    <div>Orders</div>
+    <main>
+    <PageHero title="Orders"/>
+    <Wrapper className="page section section-center">
+    {orders.map((order,index)=>{
+      return(
+        <section className="order">
+          <h4>Order #{index+1}</h4>
+          {order.map((item)=>{
+            <OrderItem key={item.id} {...item}/>
+          })}
+          <h5 className="total-bill">
+            Total bill: {formatPrice(perOrderTotal[index])}
+
+          </h5>
+        </section>
+      )
+    })}
+
+    </Wrapper>
+    </main>
   )
 }
+
+else{
+  return(
+    <main>
+      <PageHero title="Orders"/>
+      <Wrapper className="page section section-center">
+       <div className="empty">
+         <h2>You haven't purchased anything yet</h2>
+         <Link to="/products" className="btn">
+              Buy Items
+            </Link>
+       </div>
+      </Wrapper>
+    </main>
+  )
+}
+}
+
+const Wrapper=styled.section `
+   .order{
+     margin-bottom:4rem;
+   }
+
+   .total-bill{
+    text-align: right;
+    color: var(--clr-primary-4);
+    font-size: 1rem;
+    margin: 1.5rem 0;
+    font-weight: 400;
+   }
+
+   @media screen and (min-width: 776px) {
+    .total-bill {
+      font-size: 1.25rem;
+    }
+  }
+
+  .empty {
+    margin: 4rem;
+    text-align: center;
+    h2 {
+      margin-bottom: 1rem;
+      text-transform: none;
+    }
+  }
+`;
 
 export default Orders
